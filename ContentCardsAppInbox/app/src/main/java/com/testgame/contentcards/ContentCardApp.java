9e6014648a1ace503c38a03a365077587b7eb762
danielbogdanov <daniel.bogdanov@hotmail.co.uk>
2021-09-29 11:51:33 +0300

    Commiting Demo to Pulbic Repo

---
+++ ContentCardsAppInbox/app/src/main/java/com/testgame/contentcards/ContentCardApp.java
@@ -0,0 +1,23 @@
+package com.testgame.contentcards;
+
+import com.leanplum.Leanplum;
+import com.leanplum.LeanplumActivityHelper;
+import com.leanplum.LeanplumApplication;
+/**
+ * Created by Daniel Bogdanov on 09/20/21.
+ */
+
+public class ContentCardApp extends LeanplumApplication
+{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Leanplum.setApplicationContext(this);
+        LeanplumActivityHelper.enableLifecycleCallbacks(this);
+        Leanplum.setAppIdForDevelopmentMode("app_8fM8YyCDJ8Pdut5hOzT8QX46WU7Ys5xjDiotdjOqgbE", "dev_tGxYGe7FztMpyaQlWm4v4U1cC7E98H9Gy0RilLPzWlQ");
+        Leanplum.start(this);
+        Leanplum.setUserId("Dany");
+    }
+
+}
+
